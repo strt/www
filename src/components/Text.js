575d9b1257360925b3b5548e23865428bef63b3a
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-10-09 16:59:34 +0200

    Make all text fluid

--- src/components/Text.js
+++ src/components/Text.js
@@ -8,18 +8,31 @@ const text = css`
 
 export const Text = styled.p`
   ${text};
-  font-size: 1.8rem;
+  margin-bottom: 2em;
+  font-size: 1.315vw; // 20px
+  line-height: 1.6em;
 `
 
 export const Excerpt = styled.p`
   ${text};
-  font-size: 2.4rem;
+  margin-bottom: 2em;
+  font-size: 1.578vw; // 24px
   font-weight: 500;
+  line-height: 1.6em;
 `
 
 export const H1 = styled.h1`
   ${text};
-  font-size: 3.684vw;
+  margin-bottom: 0.28em;
+  font-size: 3.684vw; // 56px
   font-weight: 900;
+  line-height: 1.25em;
+`
+
+export const H2 = styled.h1`
+  ${text};
+  margin-bottom: 0.25em;
+  font-size: 2.105vw; // 32px
+  font-weight: 700;
   line-height: 1.25em;
 `
