e746891cff899b7592f7a211fa940d9c311d989d
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-10-10 10:36:20 +0200

    Improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { H1 } from '../components/Text'
+import { Grid, Column } from '../components/Grid'
 
-export default function NotFoundPage() {
+export default function NotFound() {
   return (
     <Layout title="404">
-      <p>404</p>
+      <Grid>
+        <Column>
+          <H1>404 – Sidan hittades inte.</H1>
+        </Column>
+      </Grid>
     </Layout>
   )
 }
