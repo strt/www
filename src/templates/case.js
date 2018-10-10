1dee27bde698f485eaf07b45df1e01a55aa895e9
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-10-10 08:46:25 +0200

    Add client, tags and featured image

--- src/templates/case.js
+++ src/templates/case.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link as GatsbyLink, graphql } from 'gatsby'
+import styled from 'styled-components'
 import RehypeReact from 'rehype-react'
 import Layout from '../components/Layout'
 import Link from '../components/Link'
@@ -15,16 +16,36 @@ const renderAst = new RehypeReact({
   },
 }).Compiler
 
+const Img = styled.img`
+  width: 100%;
+  margin-bottom: 2.894vw;
+`
+
 export default function Template({
   data: { markdownRemark: post },
   pageContext: { next },
 }) {
   return (
-    <Layout title={post.frontmatter.title}>
+    <Layout title={post.frontmatter.title} hideFooter>
       <Grid>
+        <Column>
+          <Text>{post.frontmatter.client}</Text>
+          <ul style={{ display: 'flex' }}>
+            {post.frontmatter.tags.map(tag => (
+              <li>
+                <Text>{tag}</Text>
+              </li>
+            ))}
+          </ul>
+        </Column>
         <Column>
           <H1>{post.frontmatter.title}</H1>
           <Excerpt>{post.frontmatter.excerpt}</Excerpt>
+        </Column>
+      </Grid>
+      <Img src={post.frontmatter.image} alt="" />
+      <Grid>
+        <Column>
           {renderAst(post.htmlAst)}
           {next && (
             <>
@@ -47,6 +68,7 @@ export const pageQuery = graphql`
       frontmatter {
         date
         title
+        client
         excerpt
         tags
         image
