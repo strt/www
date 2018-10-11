bbe030f5e038171c158a0c0363bb389817c05892
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-10-11 09:38:52 +0200

    Update start with cases and articles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,13 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import { H1, Excerpt } from '../components/Text'
+import { H1, H2, Text, Excerpt } from '../components/Text'
 import { Grid, Column } from '../components/Grid'
 import Cover from '../components/Cover'
 import Section from '../components/Section'
 import Link from '../components/Link'
 
 export default function Index({ data }) {
-  const cases = data.allMarkdownRemark.edges.map(({ node }) => ({
-    ...node,
-    ...node.frontmatter,
-    title: node.frontmatter.excerpt,
-    subtitle: node.frontmatter.title,
-    link: node.fields.slug,
-  }))
-
-  console.log(cases)
-
   return (
     <Layout
       title="Kommunikationsbyrån som gör skillnad"
@@ -47,25 +37,73 @@ export default function Index({ data }) {
           </Column>
         </Grid>
       </Section>
+      <Section>
+        <Grid>
+          <Column>
+            <H2>Case</H2>
+            {data.cases.edges.map(({ node }) => (
+              <Text key={node.fields.slug}>
+                <Link to={node.fields.slug}>
+                  {node.frontmatter.client} – {node.frontmatter.title}
+                </Link>
+              </Text>
+            ))}
+            <Link to="/case">Fler case</Link>
+          </Column>
+        </Grid>
+      </Section>
+      <Section>
+        <Grid>
+          <Column>
+            <H2>Aktuellt</H2>
+            {data.articles.edges.map(({ node }) => (
+              <Text key={node.fields.slug}>
+                <Link to={node.fields.slug}>
+                  {node.frontmatter.date} – {node.frontmatter.title}
+                </Link>
+              </Text>
+            ))}
+            <Link to="/case">Fler inlägg</Link>
+          </Column>
+        </Grid>
+      </Section>
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allMarkdownRemark(
+    cases: allMarkdownRemark(
       limit: 5
+      filter: { fileAbsolutePath: { regex: "/case/" } }
       sort: { fields: [frontmatter___date], order: DESC }
     ) {
       edges {
         node {
-          id
           fields {
             slug
           }
           frontmatter {
             title
-            excerpt
+            client
+            tags
+            image
+          }
+        }
+      }
+    }
+    articles: allMarkdownRemark(
+      limit: 4
+      filter: { fileAbsolutePath: { regex: "/aktuellt/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
             date
             image
           }
