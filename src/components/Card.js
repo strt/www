13cfe07f40856716a995ad3282d4a44973bcc450
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-10-22 11:36:17 +0200

    Add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link as GatsbyLink } from 'gatsby'
+import styled from 'styled-components'
+import { H3, Text } from './Text'
+import dayjs from '../utils/date'
+import { colors, ratio, breakpoints } from '../style'
+
+const Link = styled(GatsbyLink)`
+  text-decoration: none;
+`
+
+const Article = styled.article`
+  display: flex;
+  margin-bottom: ${32 / 15.2}vw;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: ${24 / 15.2}vw;
+  width: ${(100 / 12) * 8}%;
+  background-color: white;
+
+  h3 {
+    margin-bottom: 0;
+    text-decoration: underline;
+  }
+
+  @media ${breakpoints.medium} {
+    width: 50%;
+  }
+`
+
+const ImageWrapper = styled.div`
+  ${ratio({ x: 4, y: 3 })};
+  width: ${(100 / 12) * 4}%;
+  background-color: ${colors.gray500};
+
+  @media ${breakpoints.medium} {
+    width: 50%;
+  }
+`
+
+export default function Card({ url, title, date, image }) {
+  const formattedDate = dayjs(date).format('D MMM YYYY')
+
+  return (
+    <Link to={url}>
+      <Article>
+        <Content>
+          <Text as="time" datetime={date} textColor={colors.gray500} mb="2">
+            {formattedDate}
+          </Text>
+          <H3>{title}</H3>
+        </Content>
+        <ImageWrapper>
+          <img src={image} alt="" />
+        </ImageWrapper>
+      </Article>
+    </Link>
+  )
+}
