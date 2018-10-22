fb268c48f40c30fa7c24e2244d6a147c82f40744
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-10-22 11:38:07 +0200

    Update article listing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,10 @@ import { Grid, Column } from '../components/Grid'
 import Cover from '../components/Cover'
 import Section from '../components/Section'
 import Link from '../components/Link'
+import Card from '../components/Card'
+import Div from '../components/Div'
 import InstagramGrid from '../components/InstagramGrid'
+import { colors } from '../style'
 
 export default function Index({ data }) {
   return (
@@ -14,18 +17,17 @@ export default function Index({ data }) {
       title="Kommunikationsbyrån som gör skillnad"
       description="Välkommen till Strateg! Här finns fler än 40 strateger med en väldig massa kompetens och ett ovanligt stort engagemang."
     >
-      <Section>
+      <Section py="14">
         <Grid>
           <Column tablet="8">
             <H1>
-              Vi är en kommunikationsbyrå designing and building beautiful
-              digital products, brands, and experiences.
+              Vi är kommunikationsbyrån som älskar att ta ditt varumärke längre.
             </H1>
           </Column>
         </Grid>
       </Section>
       <Cover />
-      <Section>
+      <Section py="16">
         <Grid>
           <Column tablet="8">
             <Excerpt>
@@ -38,7 +40,7 @@ export default function Index({ data }) {
           </Column>
         </Grid>
       </Section>
-      <Section>
+      <Section py="8">
         <Grid>
           <Column>
             <H2>Case</H2>
@@ -53,25 +55,45 @@ export default function Index({ data }) {
           </Column>
         </Grid>
       </Section>
-      <Section>
+      <Section bg={colors.gray100} pt="0" pb="10">
+        <Div halfTopBg="white" mb="4">
+          <Grid>
+            <Column>
+              <H2>Aktuellt</H2>
+            </Column>
+          </Grid>
+        </Div>
         <Grid>
+          {data.articles.edges.map(({ node }) => (
+            <Column tablet="6">
+              <Card
+                date={node.frontmatter.date}
+                title={node.frontmatter.title}
+                url={node.fields.slug}
+                image={node.frontmatter.image}
+              />
+            </Column>
+          ))}
           <Column>
-            <H2>Aktuellt</H2>
-            {data.articles.edges.map(({ node }) => (
-              <Text key={node.fields.slug}>
-                <Link to={node.fields.slug}>
-                  {node.frontmatter.date} – {node.frontmatter.title}
-                </Link>
-              </Text>
-            ))}
-            <Link to="/aktuellt/">Fler inlägg</Link>
+            <Div mt="3">
+              <Link to="/aktuellt/">Fler inlägg</Link>
+            </Div>
           </Column>
         </Grid>
       </Section>
-      <Section>
+      <Section pt="5" pb="10">
         <Grid justifyContent="center">
           <Column tablet="10">
             <InstagramGrid />
+          </Column>
+        </Grid>
+      </Section>
+      <Section bg={colors.black} pt={[4, 8, 9]} pb={[6, 3, 9]}>
+        <Grid>
+          <Column>
+            <H1 as="h2" textColor="white">
+              Kontakt är det bästa vi vet
+            </H1>
           </Column>
         </Grid>
       </Section>
