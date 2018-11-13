852e93d468a87ce875c591f483d28a36f9e6cf96
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-11-13 09:07:01 +0100

    WIP career page

--- src/pages/bli-en-av-oss.js
+++ src/pages/bli-en-av-oss.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
+import Div from '../components/Div'
 import Cover from '../components/Cover'
 import Section from '../components/Section'
-import { H1, H2 } from '../components/Text'
+import InstagramGrid from '../components/InstagramGrid'
+import { H1, H2, H3, Text } from '../components/Text'
 import { Grid, Column } from '../components/Grid'
+import { breakpoints, colors } from '../style'
 
 export default function Career() {
   return (
@@ -17,13 +20,83 @@ export default function Career() {
         </H1>
       </Hero>
       <Cover id="cover" />
-      <Section>
+      <Section py="7">
         <Grid>
           <Column>
-            <H2>Loool</H2>
+            <H2>Just nu söker vi</H2>
           </Column>
         </Grid>
       </Section>
+      <Section bg={colors.dark} py="7">
+        <Grid justifyContent="space-around">
+          <Column tablet="4">
+            <H3 textColor="white">Tre annan samma äng</H3>
+            <Text textColor="white">
+              Tre annan samma äng kom se sista genom sitt sin, smultron dunge
+              bäckasiner häst hav sax göras själv kan, färdväg har bra därmed så
+              samtidigt både söka. Och där hav göras icke jäst ordningens precis
+              äng tre som blivit, bäckasiner hela vi ska söka upprätthållande
+              stora hav att lax. Varit händer sällan är ingalunda häst mjuka
+              omfångsrik rot björnbär räv, hans söka vi göras bland björnbär
+              själv ännu plats kunde, där som nya så strand miljoner.
+            </Text>
+          </Column>
+          <Column tablet="4">
+            <H3 textColor="white">Tre annan samma äng</H3>
+            <Text textColor="white">
+              Tre annan samma äng kom se sista genom sitt sin, smultron dunge
+              bäckasiner häst hav sax göras själv kan, färdväg har bra därmed så
+              samtidigt både söka. Och där hav göras icke jäst ordningens precis
+              äng tre som blivit, bäckasiner hela vi ska söka upprätthållande
+              stora hav att lax. Varit händer sällan är ingalunda häst mjuka
+              omfångsrik rot björnbär räv, hans söka vi göras bland björnbär
+              själv ännu plats kunde, där som nya så strand miljoner.
+            </Text>
+          </Column>
+        </Grid>
+        <Grid justifyContent="space-around">
+          <Column tablet="4">
+            <H3 textColor="white">Tre annan samma äng</H3>
+            <Text textColor="white">
+              Tre annan samma äng kom se sista genom sitt sin, smultron dunge
+              bäckasiner häst hav sax göras själv kan, färdväg har bra därmed så
+              samtidigt både söka. Och där hav göras icke jäst ordningens precis
+              äng tre som blivit, bäckasiner hela vi ska söka upprätthållande
+              stora hav att lax. Varit händer sällan är ingalunda häst mjuka
+              omfångsrik rot björnbär räv, hans söka vi göras bland björnbär
+              själv ännu plats kunde, där som nya så strand miljoner.
+            </Text>
+          </Column>
+          <Column tablet="4">
+            <H3 textColor="white">Tre annan samma äng</H3>
+            <Text textColor="white">
+              Tre annan samma äng kom se sista genom sitt sin, smultron dunge
+              bäckasiner häst hav sax göras själv kan, färdväg har bra därmed så
+              samtidigt både söka. Och där hav göras icke jäst ordningens precis
+              äng tre som blivit, bäckasiner hela vi ska söka upprätthållande
+              stora hav att lax. Varit händer sällan är ingalunda häst mjuka
+              omfångsrik rot björnbär räv, hans söka vi göras bland björnbär
+              själv ännu plats kunde, där som nya så strand miljoner.
+            </Text>
+          </Column>
+        </Grid>
+      </Section>
+      <Div
+        halfTopBg={colors.dark}
+        mb={[8, 19]}
+        css={{
+          '&::before': {
+            height: '64%',
+            [`@media ${breakpoints.medium}`]: { height: `${(3 / 5) * 100}%` },
+          },
+        }}
+      >
+        <Grid justifyContent="center">
+          <Column tablet="10">
+            <InstagramGrid />
+          </Column>
+        </Grid>
+      </Div>
     </Layout>
   )
 }
