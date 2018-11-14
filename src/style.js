edd3ea7c7247ae5b876972b2f0ea24584f01c84c
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-11-14 14:11:13 +0100

    Improve formatting

--- src/style.js
+++ src/style.js
@@ -68,20 +68,9 @@ export const GlobalStyle = createGlobalStyle`
   }
 `
 
-export const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-
-  to {
-    opacity: 1;
-  }
-`
-
-export const animations = {
-  fadeIn,
-}
-
+/**
+ * Mixins/helpers
+ */
 export function mediaQuery(breakpoint) {
   return `@media screen and ${breakpoint}`
 }
@@ -116,6 +105,23 @@ export function cover() {
 
 export function vw(n) {
   return `${n / 15.2}vw`
+}
+
+/**
+ * Animations
+ */
+export const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`
+
+export const animations = {
+  fadeIn,
 }
 
 /**
