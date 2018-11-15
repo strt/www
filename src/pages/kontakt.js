99a3042b0a32141696c3c02324725cd48f654d17
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-11-15 14:27:09 +0100

    Update text

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
-import { H1, Excerpt } from '../components/Text'
+import { H1, H2, Excerpt, Text } from '../components/Text'
 
 export default function Contact() {
   return (
     <Layout title="Kontakt">
       <Hero>
-        <H1>Kontakt är det bästa vi vet.</H1>
+        <H1>Välkommen till Strateg. Till det stora, svarta huset.</H1>
         <Excerpt>
-          Välkommen till Strateg. Till det stora, svarta huset. Lätt att hitta,
-          lätt att parkera vid och väldigt lätt att tycka om. Fyllt med glädje,
-          kreativitet och professionalism. Och så med alla oss strateger
-          förstås. Du tittar väl in om du har vägarna förbi?
+          Strateg Marknadsföring
+          <br />
+          Slöjdgatan 39, 703 83 Örebro
+          <br />
+          hej@strateg.se
+          <br />
+          019-764 44 00
         </Excerpt>
+        <H2>Vi finns här. Allihop.</H2>
+        <Text>
+          Vill du kontakta någon av oss skicka ett mejl till
+          fornamn.efternamn[a]strateg.se
+        </Text>
       </Hero>
     </Layout>
   )
--- src/pages/vad-vi-gor.js
+++ src/pages/vad-vi-gor.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
-import { H1 } from '../components/Text'
+import { H1, Excerpt } from '../components/Text'
 
 export default function About() {
   return (
     <Layout title="Vad vi gör">
       <Hero>
-        <H1>Vad vi gör.</H1>
+        <H1>Vi gör våra partners framgångsrika. Sedan 1985.</H1>
+        <Excerpt>
+          Söka del vid tre dimma dimmhöljd annat när, stora ännu fram plats
+          omfångsrik vidsträckt, som både därmed har om så. Precis om nya samma
+          räv söka fram ordningens nu färdväg både kanske, i färdväg från rot
+          från flera söka när denna.
+        </Excerpt>
       </Hero>
     </Layout>
   )
