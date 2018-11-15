c9bc2155fd93c6081af4266c2d925c1bdf3fc018
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-11-15 13:13:05 +0100

    Update article template

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,28 +1,37 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import { Text, Excerpt, H1 } from '../components/Text'
+import Hero from '../components/Hero'
+import Section from '../components/Section'
+import { H1, H4, Excerpt } from '../components/Text'
 import { Grid, Column } from '../components/Grid'
 import Cover from '../components/Cover'
 import renderAst from '../utils/renderAst'
+import dayjs from '../utils/date'
 
 export default function Template({ data: { markdownRemark: post } }) {
+  const formattedDate = dayjs(post.frontmatter.date).format('D MMM YYYY')
+
   return (
     <Layout title={post.frontmatter.title}>
       <article>
         <Grid>
-          <Column tablet="8">
-            <Text as="time">{post.frontmatter.date}</Text>
-            <H1>{post.frontmatter.title}</H1>
-            <Excerpt>{post.frontmatter.excerpt}</Excerpt>
+          <Column width="auto">
+            <H4 as="time">{formattedDate}</H4>
           </Column>
         </Grid>
+        <Hero pt={[2, 7]}>
+          <H1>{post.frontmatter.title}</H1>
+          <Excerpt>{post.frontmatter.excerpt}</Excerpt>
+        </Hero>
         {post.frontmatter.image && (
           <Cover>
             <img src={post.frontmatter.image} alt="" />
           </Cover>
         )}
-        <Grid>{renderAst(post.htmlAst)}</Grid>
+        <Section py="7">
+          <Grid>{renderAst(post.htmlAst)}</Grid>
+        </Section>
       </article>
     </Layout>
   )
