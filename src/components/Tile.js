7426cda627b991df3c982945e15e86b6dab7bee7
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-11-28 11:25:31 +0100

    Use scrim gradient

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -23,7 +23,22 @@ const Wrapper = styled.article`
 
 const Content = styled.div`
   padding: ${fluidRange({ min: 16, max: 24 })};
-  background-image: linear-gradient(black, transparent);
+  background-image: linear-gradient(
+    to bottom,
+    black 0%,
+    rgba(0, 0, 0, 0.738) 19%,
+    rgba(0, 0, 0, 0.541) 34%,
+    rgba(0, 0, 0, 0.382) 47%,
+    rgba(0, 0, 0, 0.278) 56.5%,
+    rgba(0, 0, 0, 0.194) 65%,
+    rgba(0, 0, 0, 0.126) 73%,
+    rgba(0, 0, 0, 0.075) 80.2%,
+    rgba(0, 0, 0, 0.042) 86.1%,
+    rgba(0, 0, 0, 0.021) 91%,
+    rgba(0, 0, 0, 0.008) 95.2%,
+    rgba(0, 0, 0, 0.002) 98.2%,
+    transparent 100%
+  );
 
   @media ${breakpoints.medium} {
     padding: ${vw(40)};
