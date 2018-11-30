2284314665fab1797851efc6a1354817e37110e8
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-11-30 10:14:14 +0100

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Link as GatsbyLink } from 'gatsby'
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
-import { H1 } from '../components/Text'
+import { H1, Excerpt } from '../components/Text'
+import Link from '../components/Link'
 
 export default function NotFound() {
   return (
     <Layout title="404">
       <Hero>
-        <H1>404 – Sidan hittades inte.</H1>
+        <H1>Ajdå.</H1>
+        <Excerpt>Vi har letat men vi hittar inte sidan du söker.</Excerpt>
+        <Link as={GatsbyLink} to="/">
+          Gå till startsidan
+        </Link>
       </Hero>
     </Layout>
   )
