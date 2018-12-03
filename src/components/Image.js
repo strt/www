ffa1be5c11feee90ded31617a60ba56bbae7ffe4
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-12-03 16:27:23 +0100

    Enable lazy image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,62 +1,152 @@
 import React from 'react'
+import styled from 'styled-components'
 import { noop } from '../utils'
+import { easings, cover } from '../style'
 
-// const cache = new Map()
-
-// const STATES = {
-//   PENDING: 'pending',
-//   LOADING: 'loading',
-//   FULFILLED: 'fulfilled',
-//   FAILED: 'failed',
-// }
-
-function DefaultErrorFallback() {
-  return <span>Error</span>
+const cache = new Map()
+function isInCache(props) {
+  if (!props.fluid) return false
+  const { src } = props.fluid
+  if (cache.has(src)) {
+    return true
+  }
+  cache.set(src, true)
+  return false
 }
 
-function DefaultLoadingFallback() {
-  return <span>Loading...</span>
+let io
+const listeners = []
+function getIntersectionObserver() {
+  if (
+    typeof io === `undefined` &&
+    typeof window !== `undefined` &&
+    window.IntersectionObserver
+  ) {
+    io = new window.IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          listeners.forEach(([element, cb]) => {
+            if (element === entry.target) {
+              if (entry.isIntersecting || entry.intersectionRatio > 0) {
+                io.unobserve(element)
+                cb()
+              }
+            }
+          })
+        })
+      },
+      { rootMargin: `200px` },
+    )
+  }
+
+  return io
 }
+
+function observeIntersections(el, cb) {
+  getIntersectionObserver().observe(el)
+  listeners.push([el, cb])
+}
+
+function normalizeProps({ fluid: image, sizes, ...props }) {
+  if (image) {
+    return {
+      src: image.src,
+      srcSet: image.srcSet,
+      sizes: sizes || image.sizes,
+      aspectRatio: image.aspectRatio,
+      base64: image.base64,
+      ...props,
+    }
+  }
+
+  return {
+    sizes,
+    ...props,
+  }
+}
+
+export const ImageWrapper = styled.figure`
+  position: relative;
+  overflow: hidden;
+  height: 0;
+  padding-bottom: ${props => `${100 / (props.aspectRatio || 2)}%`};
+
+  img {
+    max-width: auto;
+    width: 100%;
+  }
+`
+
+export const PlaceholderImage = styled.img`
+  ${cover()}
+  filter: blur(30px);
+  transform: scale(1.2);
+  z-index: 2;
+  transition: opacity 400ms ${easings.easeInSine};
+`
 
 class Image extends React.Component {
   static defaultProps = {
-    errorFallback: DefaultErrorFallback,
-    loadingFallback: DefaultLoadingFallback,
     onLoad: noop,
   }
 
-  // state = {
-  //   status: this.props.src ? STATES.LOADING : STATES.PENDING,
-  // }
+  state = {
+    isVisible: false,
+    isLoaded: false,
+    isSeenBefore: isInCache(this.props),
+  }
 
-  imageRef = React.createRef()
+  onLoad = () => {
+    this.setState({ isLoaded: true })
+    this.props.onLoad()
+  }
+
+  handleRef = (node) => {
+    if (node) {
+      observeIntersections(node, () => {
+        this.setState({ isVisible: true })
+      })
+    }
+  }
 
   render() {
-    // const { status } = this.state
-    const { src, sizes, alt = '', fluid, ...props } = this.props
-
-    if (typeof src === 'string') {
-      return <img src={src} alt="" {...props} />
-    }
+    const { isVisible, isLoaded, isSeenBefore } = this.state
+    const {
+      src,
+      sizes,
+      srcSet,
+      alt = '',
+      onLoad,
+      base64,
+      aspectRatio,
+      ...props
+    } = normalizeProps(this.props)
 
     return (
-      <img
-        srcSet={fluid.srcSet}
-        sizes={sizes || fluid.sizes}
-        src={fluid.src}
-        alt={alt}
-        {...props}
-      />
+      <ImageWrapper
+        aspectRatio={aspectRatio}
+        ref={this.handleRef}
+        key={`${JSON.stringify(srcSet)}`}
+      >
+        {base64 && (
+          <PlaceholderImage
+            src={base64}
+            alt=""
+            css={{ opacity: !isLoaded && !isSeenBefore ? 1 : 0 }}
+          />
+        )}
+        {isVisible && (
+          <img
+            alt={alt}
+            srcSet={srcSet}
+            sizes={sizes}
+            src={src}
+            onLoad={this.onLoad}
+            {...props}
+          />
+        )}
+      </ImageWrapper>
     )
-
-    // switch (status) {
-    //   case STATES.LOADED:
-    //     return <img src={this.props.src} alt={this.props.alt} />
-    //   case STATES.FAILED:
-    //     return <ErrorFallback />
-    //   default:
-    //     return <LoadingFallback />
-    // }
   }
 }
 
