66dd862683e8b43b5b6f326dc9dc0fb839b58075
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-02-14 13:48:08 +0100

    Add useMeasure

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,12 +1,26 @@
 import { useState, useRef, useEffect } from 'react'
 import createFocusTrap from 'focus-trap'
+import ResizeObserver from 'resize-observer-polyfill'
 
 export function useForceUpdate() {
   const [, setState] = useState(true)
   return () => setState(i => !i)
 }
 
-export function useToggle(defaultState = true) {
+export function useMeasure() {
+  const ref = useRef()
+  const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 })
+  const [ro] = useState(
+    () => new ResizeObserver(([entry]) => set(entry.contentRect)),
+  )
+  useEffect(() => {
+    if (ref.current) ro.observe(ref.current)
+    return () => ro.disconnect()
+  }, [])
+  return [{ ref }, bounds]
+}
+
+export function useToggle(defaultState = false) {
   const [state, setState] = useState(defaultState)
 
   return [state, () => setState(on => !on)]
