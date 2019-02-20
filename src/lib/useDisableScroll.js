d55d12256e489fc32e1c3564052f29ff4cfd0a29
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-02-20 11:06:40 +0100

    Account for scrollbar width when locking scroll

--- src/lib/useDisableScroll.js
+++ src/lib/useDisableScroll.js
@@ -1,12 +1,16 @@
 import { useEffect } from 'react'
 
-export function useDisableScroll(shouldDisable = true) {
+export default function useDisableScroll(shouldDisable = true) {
   useEffect(() => {
     if (shouldDisable) {
+      const scrollBarWidth =
+        window.innerWidth - document.documentElement.clientWidth
+      document.body.style.setProperty('margin-right', `${scrollBarWidth}px`)
       document.body.style.setProperty('overflow', 'hidden')
 
       return () => {
         document.body.style.setProperty('overflow', null)
+        document.body.style.setProperty('margin-right', null)
       }
     }
 
