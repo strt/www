065a4196d4181366cb9f9190feb6894d771cabfe
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-02-28 10:34:15 +0100

    Fix lambdas

--- lambda/src/instagram.js
+++ lambda/src/instagram.js
@@ -1,30 +1,27 @@
 import axios from 'axios'
 
-export async function handler(event, context, callback) {
+export async function handler() {
   const endpoint = 'https://api.instagram.com/v1/users/self/media/recent'
   const token = process.env.INSTAGRAM_ACCESS_TOKEN
   const limit = 5
 
-  axios
-    .get(`${endpoint}?access_token=${token}&count=${limit}`)
-    .then(({ data: { data: posts } }) => {
-      callback(null, {
-        statusCode: 200,
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify(
-          posts.map(i => ({
-            id: i.id,
-            link: i.link,
-            images: i.images,
-            videos: i.videos,
-            caption: i.caption.text,
-          })),
-        ),
-      })
-    })
-    .catch((e) => {
-      callback(e)
-    })
+  const {
+    data: { data: posts },
+  } = await axios.get(`${endpoint}?access_token=${token}&count=${limit}`)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(
+      posts.map(i => ({
+        id: i.id,
+        link: i.link,
+        images: i.images,
+        videos: i.videos,
+        caption: i.caption.text,
+      })),
+    ),
+  }
 }
