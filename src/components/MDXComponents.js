75c1d52097df4e35e2038dd7934d1efba3bc5671
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-03-22 08:13:16 +0100

    Custom wrapper no longer needed

--- src/components/MDXComponents.js
+++ src/components/MDXComponents.js
@@ -1,11 +1,9 @@
-import React from 'react'
 import { H1, H2, H3, Text } from './Text'
 import Link from './Link'
 import { Column } from './Grid'
 import Image from './Image'
 
 const components = {
-  wrapper: React.Fragment,
   h1: H1,
   h2: H2,
   h3: H3,
