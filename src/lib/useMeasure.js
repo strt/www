76ef33653337455ccec447bbd7b02ff9e3a8a2da
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-03-25 08:12:58 +0100

    Rename variable

--- src/lib/useMeasure.js
+++ src/lib/useMeasure.js
@@ -4,12 +4,12 @@ import ResizeObserver from 'resize-observer-polyfill'
 export default function useMeasure() {
   const ref = useRef(null)
   const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 })
-  const [ro] = useState(
+  const [observer] = useState(
     () => new ResizeObserver(([entry]) => set(entry.contentRect)),
   )
   useEffect(() => {
-    if (ref.current) ro.observe(ref.current)
-    return () => ro.disconnect()
-  }, [])
+    if (ref.current) observer.observe(ref.current)
+    return () => observer.disconnect()
+  }, [observer])
   return [{ ref }, bounds]
 }
