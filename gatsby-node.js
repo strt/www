f0448b933303bcfd3a125790a4398783b4118716
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-03-25 08:27:54 +0100

    Remove limit

--- gatsby-node.js
+++ gatsby-node.js
@@ -60,7 +60,6 @@ exports.createPages = async ({ actions, graphql }) => {
   const result = await graphql(`
     {
       allMdx(
-        limit: 1000
         sort: { order: DESC, fields: [frontmatter___date] }
         filter: {
           frontmatter: { published: { ne: false } }
