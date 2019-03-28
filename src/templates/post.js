8ea1da93f89952f779d669730a5e9415256d4ab1
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-03-28 16:29:34 +0100

    Add post listing

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,11 +6,16 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import Image from '../components/Image'
 import Section from '../components/Section'
-import { H1, H4, Excerpt } from '../components/Text'
+import { H1, H2, H4, Excerpt } from '../components/Text'
 import { Grid, Column } from '../components/Grid'
 import ContentWrapper from '../components/ContentWrapper'
 import Cover from '../components/Cover'
+import Div from '../components/Div'
+import Link from '../components/Link'
+import Card from '../components/Card'
 import getMetaFromPost from '../lib/getMetaFromPost'
+import { colors } from '../style'
+import { routes } from '../routes'
 
 export default function Article({ data }) {
   const { date, title, excerpt, image } = data.post.frontmatter
@@ -49,6 +54,34 @@ export default function Article({ data }) {
             </Grid>
           </ContentWrapper>
         </Section>
+        <Section as="aside" bg={colors.ice} mt={[10, 15]} pb={[5, 12]}>
+          <Div halfTopBg="white" mb={[2, 4]}>
+            <Grid>
+              <Column>
+                <H2>News</H2>
+              </Column>
+            </Grid>
+          </Div>
+          <Grid>
+            {data.posts.edges.map(({ node }) => (
+              <Column key={node.id} md="6" bottomGap>
+                <Card
+                  date={node.frontmatter.date}
+                  title={node.frontmatter.title}
+                  url={node.fields.slug}
+                  image={node.frontmatter.image}
+                />
+              </Column>
+            ))}
+            <Column>
+              <Div mt={[3, 2]}>
+                <Link to={routes.news.link} variant="large">
+                  More news
+                </Link>
+              </Div>
+            </Column>
+          </Grid>
+        </Section>
       </article>
     </Layout>
   )
@@ -72,6 +105,32 @@ export const pageQuery = graphql`
             ...CoverImage
             og: resize(width: 1200, height: 630, quality: 80) {
               src
+            }
+          }
+        }
+      }
+    }
+    posts: allMdx(
+      limit: 4
+      filter: {
+        fields: { template: { eq: "post" }, slug: { ne: $slug } }
+        frontmatter: { published: { ne: false } }
+      }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date
+            image {
+              childImageSharp {
+                ...CardImage
+              }
             }
           }
         }
