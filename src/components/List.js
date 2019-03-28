f070652701e710e7d87013c5e1bf0d63b9b9642f
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-03-28 14:22:30 +0100

    Add some margin

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,11 @@
 import styled, { css } from 'styled-components'
 import { textSize } from './Text'
+import { fluidRange } from '../style'
 
 const style = css`
   ${textSize}
   margin-bottom: 2em;
+  margin-left: ${fluidRange({ min: 4, max: 8 })};
   list-style-position: inside;
   line-height: 1.6em;
 `
