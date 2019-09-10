72ddb07229da6632d73f09fa13cda23e163384fa
Patrik Nilsson <patriknilsson1992@gmail.com>
2019-09-10 11:56:57 +0200

    for Rich content add custom styled components

--- src/components/RichTextContentful.js
+++ src/components/RichTextContentful.js
@@ -1,8 +1,39 @@
 /* eslint react/forbid-prop-types: 0 */
 import PropTypes from 'prop-types'
+import React from 'react'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { BLOCKS, INLINES } from '@contentful/rich-text-types'
+import { H1, H2, H3, Text } from './Text'
+import { UnorderedList } from './List'
+import Link from './Link'
 
-const options = {}
+function isUrlAbsolute(url) {
+  return url.indexOf('://') > 0 || url.indexOf('//') === 0
+}
+
+const options = {
+  renderNode: {
+    [BLOCKS.HEADING_1]: (node, children) => <H1>{children}</H1>,
+    [BLOCKS.HEADING_2]: (node, children) => <H2>{children}</H2>,
+    [BLOCKS.HEADING_3]: (node, children) => <H3>{children}</H3>,
+    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+    [INLINES.HYPERLINK]: (node, children) => {
+      const isAbsolute = isUrlAbsolute(node.data.uri)
+      return (
+        <Link
+          rel={isAbsolute ? 'nfollow,noopener,noreferrer' : ''}
+          blank={isAbsolute ? '_blank' : ''}
+          href={node.data.uri}
+        >
+          {children}
+        </Link>
+      )
+    },
+    [BLOCKS.UL_LIST]: (node, children) => (
+      <UnorderedList>{children}</UnorderedList>
+    ),
+  },
+}
 
 const RichText = ({ document }) => {
   return documentToReactComponents(document, options)
