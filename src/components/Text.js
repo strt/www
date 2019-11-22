5c81e93ba4216762ecde34322c2e1d7794f630a4
Johan Tor <johan.tor@gmail.com>
2019-11-22 16:21:22 +0100

    Update of text sizes

--- src/components/Text.js
+++ src/components/Text.js
@@ -3,10 +3,10 @@ import {
   colors,
   fontFamily,
   breakpoints,
+  breakpointNr,
   fluidRange,
   textAlign,
   space,
-  vw,
 } from '../style'
 
 export const base = css(
@@ -19,11 +19,7 @@ export const base = css(
 )
 
 export const textSize = css`
-  font-size: ${fluidRange({ min: 14, max: 18 })};
-
-  @media ${breakpoints.medium} {
-    font-size: max(${vw(20)}, 10px);
-  }
+  font-size: 16px;
 `
 
 export const Text = styled.p`
@@ -32,93 +28,185 @@ export const Text = styled.p`
   line-height: 1.5em;
   ${base}
   ${textSize}
+
+  @media ${breakpoints.medium} {
+    font-size: 1.25em;
+  }
+
+  @media ${breakpoints.large} {
+    font-size: ${fluidRange({
+      min: 20,
+      max: 30,
+      viewportMin: breakpointNr.large,
+      viewportMax: breakpointNr.xlarge,
+    })};
+  }
+
+  @media ${breakpoints.xlarge} {
+    font-size: 1.875em;
+  }
 `
 
 export const Excerpt = styled.p`
   margin-bottom: 1.25em;
-  font-size: ${fluidRange({ min: 16, max: 20 })};
+  font-size: 1.875em;
   font-weight: 500;
   line-height: 1.25em;
+  white-space: pre-line;
   ${base}
 
   @media ${breakpoints.medium} {
-    font-size: max(${vw(24)}, 12px);
+    max-width: 50%;
+    font-size: 2.5em;
+  }
+
+  @media ${breakpoints.large} {
+    font-size: ${fluidRange({
+      min: 40,
+      max: 60,
+      viewportMin: breakpointNr.large,
+      viewportMax: breakpointNr.xlarge,
+    })};
+  }
+
+  @media ${breakpoints.xlarge} {
+    font-size: 3.75em;
+  }
+
+  a::after {
+    bottom: -10%;
+    background-size: 9px 11px;
+
+    @media ${breakpoints.large} {
+      bottom: 0%;
+      background-size: 15px 11px;
+    }
   }
 `
 
 export const H1 = styled.h1`
   margin-bottom: 0.175em;
-  font-size: 1.75em;
+  font-size: 4em;
   font-weight: 400;
-  line-height: 1.125em;
-  ${base} 
-  
-  @media ${breakpoints.medium} {
-    font-size: 2.5em;
-    line-height: 1.125em;
-  }
-`
-
-export const H1Large = styled.h1`
-  margin-bottom: 0.175em;
-  font-size: ${fluidRange({ min: 48, max: 64 })};
-  font-weight: 400;
-  line-height: 1.25em;
+  line-height: 1.1em;
   ${base}
 
   @media ${breakpoints.medium} {
     font-size: 8.25em;
-    line-height: 0.924em;
-    letter-spacing: -6.6px;
+    letter-spacing: -0.05em;
+  }
+
+  @media ${breakpoints.large} {
+    font-size: ${fluidRange({
+      min: 132,
+      max: 196,
+      viewportMin: breakpointNr.large,
+      viewportMax: breakpointNr.xlarge,
+    })};
+  }
+
+  @media ${breakpoints.xlarge} {
+    font-size: 12.25em;
   }
 `
 
 export const H2 = styled.h2`
-  margin-bottom: 0.15625em;
-  font-size: ${fluidRange({ min: 24, max: 28 })};
+  margin-bottom: 0.4125em;
+  font-size: 1.875em;
   font-weight: 700;
-  line-height: 0.78125em;
+  line-height: 1.25em;
   ${base}
 
   @media ${breakpoints.medium} {
-    font-size: max(${vw(32)}, 14px);
+    font-size: 2.5em;
+  }
+
+  @media ${breakpoints.large} {
+    font-size: ${fluidRange({
+      min: 40,
+      max: 60,
+      viewportMin: breakpointNr.large,
+      viewportMax: breakpointNr.xlarge,
+    })};
+  }
+
+  @media ${breakpoints.xlarge} {
+    font-size: 3.75em;
   }
 `
 
 export const H3 = styled.h3`
   margin-bottom: 0.4125em;
-  font-size: ${fluidRange({ min: 18, max: 24 })};
-  font-weight: 500;
-  line-height: 0.9em;
+  font-size: 1.5em;
+  font-weight: 700;
+  line-height: 1.125em;
   ${base}
 
   @media ${breakpoints.medium} {
-    font-size: max(${vw(24)}, 12px);
+    font-size: 2em;
+  }
+
+  @media ${breakpoints.large} {
+    font-size: ${fluidRange({
+      min: 32,
+      max: 48,
+      viewportMin: breakpointNr.large,
+      viewportMax: breakpointNr.xlarge,
+    })};
+  }
+
+  @media ${breakpoints.xlarge} {
+    font-size: 3em;
   }
 `
 
 export const H4 = styled.h4`
   margin-bottom: 0.4125em;
-  font-size: ${fluidRange({ min: 14, max: 18 })};
+  font-size: 1.3125em;
   font-weight: 700;
-  line-height: 0.78125em;
+  line-height: 1.25em;
   ${base}
 
   @media ${breakpoints.medium} {
-    font-size: max(${vw(20)}, 10px);
+    font-size: 1.75em;
+  }
+
+  @media ${breakpoints.large} {
+    font-size: ${fluidRange({
+      min: 28,
+      max: 42,
+      viewportMin: breakpointNr.large,
+      viewportMax: breakpointNr.xlarge,
+    })};
+  }
+
+  @media ${breakpoints.xlarge} {
+    font-size: 2.625em;
   }
 `
 
 export const H6 = styled.h6`
 margin-bottom: 0.4125em;
-font-size: ${fluidRange({ min: 12, max: 15 })};
-  font-weight: 700;
-  letter-spacing: 3px;
-  text-transform: uppercase;
-  line-height: 0.78125em;
-  ${base}
+font-size: 1.3125em;
+font-weight: 700;
+text-transform: uppercase;
+line-height: 1em;
+${base}
 
-  @media ${breakpoints.medium} {
-    font-size: max(${vw(14)}, 10px);
-  }
+@media ${breakpoints.medium} {
+  font-size: 1.25em;
+}
+
+@media ${breakpoints.large} {
+  font-size: ${fluidRange({
+    min: 22,
+    max: 33,
+    viewportMin: breakpointNr.large,
+    viewportMax: breakpointNr.xlarge,
+  })};
+}
+
+@media ${breakpoints.xlarge} {
+  font-size: 2.0625em;
+}
 `
