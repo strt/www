b40ee1890371b5b6332568f582e1834562d25782
Dan Godiac <dan.godiac@strateg.se>
2020-08-10 15:48:16 +0200

    new instagram endpoint

--- lambda/src/instagram.js
+++ lambda/src/instagram.js
@@ -1,13 +1,34 @@
 import axios from 'axios'
 
 export async function handler() {
-  const endpoint = 'https://api.instagram.com/v1/users/self/media/recent'
+  const endpoint = 'https://graph.instagram.com'
   const token = process.env.INSTAGRAM_ACCESS_TOKEN
-  const limit = 5
 
   const {
     data: { data: posts },
-  } = await axios.get(`${endpoint}?access_token=${token}&count=${limit}`)
+  } = await axios.get(`${endpoint}/me/media?access_token=${token}&fields=id,media_url,permalink,caption,media_type`)
+
+  const getFirstAlbumMedia = async function (id) {
+    const {
+      data: { data: children },
+    } = await axios.get(`${endpoint}/${id}/children?access_token=${token}&fields=id,media_url,permalink,media_type`)
+
+    if (children.length) {
+      return children[0]
+    }
+
+    return {}
+  }
+
+  const resolveMediaType = function (item) {
+    if (item.mediaType === 'CAROUSEL_ALBUM') {
+      const childMediaType = getFirstAlbumMedia(item.id).media_type
+
+      return childMediaType === 'VIDEO' ? 'video' : 'image'
+    }
+
+    return item.mediaType === 'VIDEO' ? 'video' : 'image'
+  }
 
   return {
     statusCode: 200,
@@ -17,9 +38,9 @@ export async function handler() {
     body: JSON.stringify(
       posts.map(i => ({
         id: i.id,
-        link: i.link,
-        images: i.images,
-        videos: i.videos,
+        link: i.permalink,
+        media_url: i.media_url,
+        media_type: resolveMediaType(i),
         caption: i.caption.text,
       })),
     ),
