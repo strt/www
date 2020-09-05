038caeb9090e610b2a3db145826bc367a1e04b2f
Dan Godiac <dan.godiac@strateg.se>
2020-09-05 23:34:18 +0200

    Added token refreshing mechanism

--- lambda/src/instagram.js
+++ lambda/src/instagram.js
@@ -3,15 +3,40 @@ import axios from 'axios'
 export async function handler() {
   const endpoint = 'https://graph.instagram.com'
   const token = process.env.INSTAGRAM_ACCESS_TOKEN
+  const date = new Date()
+
+  const refreshToken = async function() {
+    await axios.get(`${endpoint}/refresh_access_token`, {
+      params: {
+        access_token: token,
+        grant_type: 'ig_refresh_token',
+      },
+    })
+  }
+
+  // Update the token every first day of the month
+  if (date.getDate() === 1) {
+    await refreshToken()
+  }
 
   const {
     data: { data: posts },
-  } = await axios.get(`${endpoint}/me/media?access_token=${token}&fields=id,media_url,permalink,caption,media_type`)
+  } = await axios.get(`${endpoint}/me/media`, {
+    params: {
+      access_token: token,
+      fields: 'id,media_url,permalink,caption,media_type',
+    },
+  })
 
-  const getFirstAlbumMedia = async function (id) {
+  const getFirstAlbumMedia = async function(id) {
     const {
       data: { data: children },
-    } = await axios.get(`${endpoint}/${id}/children?access_token=${token}&fields=id,media_url,permalink,media_type`)
+    } = await axios.get(`${endpoint}/${id}/children`, {
+      params: {
+        access_token: token,
+        fields: 'id,media_url,permalink,media_type',
+      },
+    })
 
     if (children.length) {
       return children[0]
@@ -20,7 +45,7 @@ export async function handler() {
     return {}
   }
 
-  const resolveMediaType = function (item) {
+  const resolveMediaType = function(item) {
     if (item.media_type === 'CAROUSEL_ALBUM') {
       const childMediaType = getFirstAlbumMedia(item.id).media_type
 
