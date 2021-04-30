1992915d8ce1128eb012130a957433ca390b00c2
Jonas Näsman <55234353+MrjonasN@users.noreply.github.com>
2021-04-30 10:52:28 +0200

    Develop (#99)
    * cookieToast styling
    
    * cookieToast mobile styling
    
    * Styling on cookie toast
    
    * added ga cookies if cookie consent
    
    * checking if accept cookies is true
    
    * Fix/cookies gdpr (#98)
    
    * cookieToast styling
    
    * cookieToast mobile styling
    
    * Styling on cookie toast
    
    * added ga cookies if cookie consent
    
    * checking if accept cookies is true
    
    Co-authored-by: Dan Godiac <dan.godiac@strateg.se>
    Co-authored-by: Jonas Näsman <jonasnasman@Linnea-Bengtz-MacBook-Pro.local>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Cookie from 'js-cookie'
 import { ThemeProvider } from './src/context/ThemeContext'
 
 require('focus-visible')
@@ -6,3 +7,11 @@ require('focus-visible')
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider>{element}</ThemeProvider>
 )
+
+export const onClientEntry = () => {
+  window[`ga-disable-${process.env.GOOGLE_ANALYTICS_ID}`] = true
+
+  if (Cookie.get('accept_cookies') === 'true') {
+    window[`ga-disable-${process.env.GOOGLE_ANALYTICS_ID}`] = false
+  }
+}
--- src/components/CookieToast.js
+++ src/components/CookieToast.js
@@ -2,32 +2,61 @@ import React, { useState, useEffect } from 'react'
 import { useTransition, animated } from 'react-spring'
 import styled from 'styled-components'
 import Cookie from 'js-cookie'
-import Link from './Link'
 import { Grid, Column } from './Grid'
-import { Text } from './Text'
-import { colors, vw, breakpoints, fluidRange } from '../style'
-import { routes } from '../routes'
-import { getActiveLangPath, isDefaultLanguage } from './SelectLanguage'
+import { TextSmall, H4 } from './Text'
+import { colors, breakpoints } from '../style'
+import { isDefaultLanguage } from './SelectLanguage'
+import Button from './Button'
 
 const CookieToastWrapper = animated(styled.div`
   position: fixed;
   right: 0;
   bottom: 0;
-  left: 0;
   z-index: 3;
   backface-visibility: hidden;
 `)
 
 const Toast = styled.div`
-  padding: ${fluidRange({ min: 24, max: 32 })} 0;
-  margin-right: ${fluidRange({ min: -16, max: -24 })};
-  margin-left: ${fluidRange({ min: -16, max: -24 })};
-  background-color: ${colors.dark};
+  max-width: 480px;
+  padding: 1rem 0;
+  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2), 0 4px 8px 0 rgba(0, 0, 0, 0.1);
+  background-color: ${colors.grey100};
 
-  @media ${breakpoints.medium} {
-    padding: ${vw(32)} 0;
-    margin-right: 0;
-    margin-left: 0;
+  h4 {
+    font-size: 1.125rem;
+  }
+
+  p {
+    margin-top: 0.8rem;
+    margin-bottom: 2.5rem;
+    font-size: 1rem;
+  }
+
+  .cookie-button {
+    padding: 0.8rem 0.8rem 0.9rem;
+    width: 100%;
+    font-size: 1rem;
+    cursor: pointer;
+
+    @media ${breakpoints.small} {
+      width: initial;
+    }
+
+    &--dark {
+      border: 1px solid ${colors.darkText};
+    }
+
+    &--light {
+      margin-top: 0.7rem;
+      border: 1px solid ${colors.darkText};
+      color: ${colors.darkText};
+      background-color: ${colors.light};
+
+      @media ${breakpoints.small} {
+        margin-top: 0;
+        margin-left: 0.5rem;
+      }
+    }
   }
 `
 
@@ -63,34 +92,48 @@ export default function CookieToast() {
                   alignItems={['flex-end', 'flex-end', 'center']}
                 >
                   <Column>
-                    <Text textColor={colors.light} m="0">
+                    <H4 textColor={colors.darkText} m="0">
                       {isDefaultLanguage()
-                        ? 'Vi använder oss av kakor. Mums. '
+                        ? 'Den här webbplatsen använder cookies. Mums. '
                         : 'This website uses cookies. Yum. '}
-                      <Link
+                    </H4>
+                    <TextSmall>
+                      {isDefaultLanguage()
+                        ? 'Vi använder nödvändiga cookies för att vår webbplats ska fungera och analytiska cookies för trafikmätning som hjälper oss att förbättra upplevelsen. '
+                        : 'We use necessary cookies for our website to function and analytical cookies for measuring traffic which helps us with optimization of our content. '}
+                      {/* <Link
                         to={`${getActiveLangPath()}/${routes.policy.link}`}
-                        textColor={colors.light}
+                        textColor={colors.linkDark}
                         styleVariant="dark"
                       >
                         {isDefaultLanguage() ? 'Läs mer' : 'Read more'}
-                      </Link>
-                      .
-                    </Text>
-                  </Column>
-                  <Column width="auto">
-                    <Link
+                      </Link> */}
+                    </TextSmall>
+                    <Button
                       as="button"
                       type="button"
-                      textColor={colors.light}
-                      styleVariant="dark"
-                      variant="large"
+                      className="cookie-button cookie-button--dark"
+                      textColor={colors.darkText}
                       onClick={() => {
                         setShowToast(false)
                         Cookie.set('accept_cookies', true, { expires: 365 })
                       }}
                     >
-                      OK
-                    </Link>
+                      {isDefaultLanguage()
+                        ? 'Acceptera cookies '
+                        : 'Accept cookies '}
+                    </Button>
+                    {/* <Button
+                      as="button"
+                      type="button"
+                      className="cookie-button cookie-button--light"
+                      textColor={colors.darktext}
+                      onClick={() => {
+                        setShowToast(true);
+                      }}
+                    >
+                      {isDefaultLanguage() ? 'Inställningar' : 'Settings'}
+                    </Button> */}
                   </Column>
                 </Grid>
               </Toast>
