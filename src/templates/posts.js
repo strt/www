8847ce82a66c98e74f4fbd6067fca6866c3a2433
MrJonasN <jonas.nasman87@gmail.com>
2021-05-12 16:14:49 +0200

    Revert "Revert "added focus to load more element""
    This reverts commit 2b6a849344bb94edeb9bc0b8850137ffe3d30537.

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -15,13 +15,16 @@ import getMetaFromPost from '../lib/getMetaFromPost'
 export default function News({ data }) {
   const [limit, setLimit] = useState(8)
 
-  const loadMore = () => {
-    setLimit(prevLimit => prevLimit + 8)
-  }
-
   const { name, excerpt } = data.contentfulPage
 
+  const handleKeyUp = e => {
+    if (e.keyCode === 13) {
+      setLimit(prevLimit => prevLimit + 8)
+    }
+  }
+
   const LoadMoreDiv = styled.div`
+    display: inline-block;
     cursor: pointer;
     color: ${colors.linkDark};
     font-size: 1.125rem;
@@ -30,6 +33,12 @@ export default function News({ data }) {
     &:focus {
       text-decoration: underline;
       opacity: 0.85;
+    }
+
+    &:focus-visible {
+      outline: none;
+      text-decoration: none;
+      background-color: ${colors.orange300};
     }
 
     &:active,
@@ -86,8 +95,12 @@ export default function News({ data }) {
               style={{
                 display: limit >= data.articles.edges.length ? 'none' : '',
               }}
-              onClick={loadMore}
+              onClick={() => {
+                setLimit(prevLimit => prevLimit + 8)
+              }}
+              onKeyUp={handleKeyUp}
               textColor={theme.linkColor}
+              tabIndex="0"
             >
               Visa fler
             </LoadMoreDiv>
